39f0f90c4f786b87158ca6f0664b5289c8989a26
notech <996635852@163.com>
2014-11-24 00:24:52 +0800

    a

--- app/controllers/PostController.php
+++ app/controllers/PostController.php
@@ -4,10 +4,10 @@ class PostController extends \controller
 {
 	public function index($id = 1){
 		if($id){
-			$s = Post::findOrFail($id);
+			$post = Post::findOrFail($id);
 		}else{
-			$s = DB::table('posts')->get();
+			$post = DB::table('posts')->get();
 		}
-		return Response::json($s);
+		return View::make('font.post')->withPost($post);
 	}
 }
---
+++ app/views/font/post.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+	<meta charset="UTF-8">
+	<title>{{$post->post_title}} - 内蒙古天增寺</title>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
+	<meta name="format-detection" content="telephone=no">
+	<meta http-equiv="Cache-Control" content="no-siteapp"/>
+	<link rel="alternate icon" type="image/png" href="assets/i/favicon.png">
+	<link rel="stylesheet" href="http://notech.qiniudn.com/amazeui.min.css"/>
+	<link rel="stylesheet" href="http://notech.qiniudn.com/postindex.css">
+</head>
+<body>
+	<div class="topnav">
+		<div class="topnav_c">
+			<a class="logo" href="#"><img src="http://notech.qiniudn.com/logo.gif" alt=""/></a>
+			<div class="toplinks">
+				<a href="../index.html">天增寺1</a>
+				<a href="../index.html">天增寺2</a>
+				<a href="../index.html">天增寺3</a>
+				<a href="../index.html">天增寺4</a>
+				<a href="../index.html">天增寺5</a>
+				<a href="../index.html">天增寺6</a>
+			</div>
+		</div>
+	</div>
+	<div class="content">
+		<div class="cont_box">
+			<h3 class="desc_title">{{$post->post_title}}</h3>
+			<div class="desc_text">
+				<p>{{$post->post_content}}</p>
+			</div>
+			<div class="imgwrap"><img src="http://www.kamakura-zuisenji.or.jp/imgs/garden/p1.jpg" alt=""/></div>
+		</div>
+		<div class="cont_box">
+			<h3 class="desc_title">国師はその著「夢中問答」</h3>
+			<div class="desc_text">
+				<p>国師はその著「夢中問答」で、山水（庭園）を愛するのは「泉石草木の四気にかわる気色を工夫とする」、つまり自身の心を磨く修行のためと述べておられます。難しいことはさておき、夢窓国師のこの庭園に対した時、あるいはこの境内全域に包まれている時、私たちは七朝の帝師の時空を超えた説法を、知らず聞いているのです。</p>
+				<p>なお、瑞泉寺境内を含む周辺地域は、鎌倉時代からの歴史的風土がよく保存されているところから、国の史跡瑞泉寺境内、また古都保存法による歴史的風土特別保存地区に指定されております。</p>
+			</div>
+			<div class="imgwrap"><img src="http://www.kamakura-zuisenji.or.jp/imgs/garden/p3.jpg" alt=""/></div>
+		</div>
+	</div>
+	<div class="footer">
+		<p class="footer_c">
+			<span id="copyright">Copyright (©) 天增寺 All rights reserved.</span>
+			<span id="produced">Produced by</span>
+			<a href="http://www.notech.net" target="_blank">notech没技术</a>
+		</p>
+	</div>
+</body>
+<script src="assets/js/zepto.min.js?ver=1.1.3"></script>
+<script src="assets/js/amazeui.js"></script>
+<script src="assets/js/handlebars.min.js"></script>
+<script src="assets/js/amazeui.widgets.helper.js"></script>
+</html>
